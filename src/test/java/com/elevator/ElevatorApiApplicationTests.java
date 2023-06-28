b183cbac22c8fbbaa8525d8a5cbaf886ac937a8b
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2023-06-28 18:29:41 +0300

    initial commit

---
+++ src/test/java/com/elevator/ElevatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elevator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElevatorApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
