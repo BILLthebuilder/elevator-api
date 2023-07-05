8fb9f50a0161ffd8f9ef775cb1cb23d5c12862b8
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2023-07-05 12:03:42 +0300

    add property to find by ID

--- src/main/java/com/elevator/repository/ElevatorRepository.java
+++ src/main/java/com/elevator/repository/ElevatorRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface ElevatorRepository extends JpaRepository<Elevator, Long> {
+
+    Elevator findById(UUID id);
 }
